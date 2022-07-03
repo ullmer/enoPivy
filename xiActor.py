24b6f217a52fce17095c64bac59557640a493654
Brygg Ullmer <bullmer@clemson.edu>
2022-07-03 07:02:13 -0400

    eXtended Interaction ensemble stubs

---
+++ xiActor.py
@@ -0,0 +1,6 @@
+# Placeholder for Xi (eXtended Interaction) Actor proxy
+# Abstracts whether instances are PyGame Zero Actors; Pivy-Xi Actors; Project Zanzibar tangibles;
+# or ensemble permutations of or beyond these.
+
+# Brygg Ullmer, Clemson University
+# Begun 2022-07-03
---
+++ xiStage.py
@@ -0,0 +1,6 @@
+# Placeholder for Xi (eXtended Interaction) Stage proxy
+# Abstracts whether instances are pgzero-xi Stage; pivy-xi Actors; Project Zanzibar workspace;
+# or ensemble permutations of or beyond these.
+
+# Brygg Ullmer, Clemson University
+# Begun 2022-07-03
