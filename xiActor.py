26a43cbce7314f18d8d16885a86a1936c5590295
Brygg Ullmer <bullmer@clemson.edu>
2022-07-03 07:05:50 -0400

    More framing comments

--- xiActor.py
+++ xiActor.py
@@ -1,6 +1,7 @@
 # Placeholder for Xi (eXtended Interaction) Actor proxy
 # Abstracts whether instances are PyGame Zero Actors; Pivy-Xi Actors; Project Zanzibar tangibles;
-# or ensemble permutations of or beyond these.
+# or ensemble permutations of or beyond these.  One goal is to preserve pgzero Actor methods 
+# as closely as possible, toward minimizing learning curve and interoperability 
 
 # Brygg Ullmer, Clemson University
 # Begun 2022-07-03
