3b8cf7a1fe0da1e076431083b9461eb0df668da3
Brygg Ullmer <bullmer@clemson.edu>
2022-07-03 06:58:20 -0400

    Proxies for tangibles within Microsoft Project Zanzibar, following PyGame Zero Actor syntax and ~semantics

---
+++ pzxActor.py
@@ -0,0 +1,2 @@
+# Proxy for tangibles within Microsoft Project Zanzibar
+# https://www.microsoft.com/en-us/research/project/project-zanzibar/
---
+++ pzxStage.py
@@ -0,0 +1,2 @@
+# Proxy for workspace sensing mat in Microsoft Project Zanzibar
+# https://www.microsoft.com/en-us/research/project/project-zanzibar/
