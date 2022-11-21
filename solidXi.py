44fc68811f940e10fec4dadd65d8c61fbc7226dc
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2022-11-21 15:05:42 -0500

    first steps

---
+++ solidXi.py
@@ -0,0 +1,15 @@
+# Extended interaction (Xi, per XR) enhancements to SolidPython
+# Brygg Ullmer, Clemson University
+# Begun 2022-11-21
+
+from solid import *
+from solid.utils import *
+
+class XCube(Cube)
+  def __add__(self, o):
+
+    if o.type
+
+OpenSCADObject
+
+### end ###
